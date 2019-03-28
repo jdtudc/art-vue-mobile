ac0ee2feb835d321cc73d6ce2162cae510acba5f
McChen <chenjiahao@jd.com>
2019-03-28 16:06:26 +0800

    chore: add event tracking

--- examples/main.js
+++ examples/main.js
@@ -4,6 +4,9 @@ import router from './router'
 
 import artm from '../src/index'
 
+import FastClick from 'fastclick'
+FastClick.attach(document.body)
+
 Vue.config.productionTip = false
 
 Vue.use(artm)
